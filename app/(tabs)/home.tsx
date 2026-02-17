047a642372c336171e3f7de921ea337829caf965
Santiago pabon <santiago.pabon72@gmail.com>
2026-02-16 23:06:09 -0700

    added filtering based when iterating . added styling to components within gamecard component

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,12 +1,15 @@
 import  OverViewCard from "@/components/homeComponents/OverViewCard";
 import { StyleSheet, Text, View } from "react-native";
+import games from "@/data/games.json";
 
 export default function Home() {
   return (
     <View>
       <Text>Home Screen</Text>
       <View style={styles.OverViewCard}>
-        <OverViewCard />
+        {games.filter((game) => game.isFavorite === true).map((game)=> (
+            <OverViewCard key={game.id} {...game}/>
+        ))}
       </View>
     </View>
   );
--- components/homeComponents/OverViewCard.tsx
+++ components/homeComponents/OverViewCard.tsx
@@ -1,6 +1,7 @@
 
 import { View, StyleSheet,Text } from "react-native";
 import CircleButton from "../ui/CircleButton";
+import { Image } from "expo-image";
 
 
 type Props = {
@@ -18,12 +19,14 @@ type Props = {
 export default function OverViewCard(props : Props){
     return(
         <View style={styles.background}>
-        <Text>{props.gameDate}</Text>
+        <Text style={styles.gameDateStyle}>{props.gameDate}</Text>
         <Text>{props.teamA}</Text>
         <Text>{props.teamB}</Text>
-        <Text>{props.gameTime}</Text>
+        <Text style={styles.gameTimeStyle}>{props.gameTime.trim().substring(0,5)}</Text>
         <CircleButton style={styles.CompCircleButtonStyle} iconName="football-outline" iconSize={20}/>
         <CircleButton style={styles.MoreOptionsCircleButtonStyle} iconName="ellipsis-vertical" iconSize={20}/>
+        <Image source={{uri: props.teamALogoUrl}} style={styles.teamALogoStyle}/>
+        <Image source={{uri: props.teamBLogoUrl}} style={styles.teamBLogoStyle}/>
         </View>
     );
 };
@@ -48,6 +51,37 @@ const styles = StyleSheet.create({
         left: 310,
         top: 10,
         backgroundColor: "black"
-
-    }
+    },
+    gameDateStyle: {
+        position: "absolute",
+        color: "white",
+        textAlign: "center",
+        width: "100%",
+        top: 48,
+        fontSize: 11,
+        fontWeight: "bold"
+    },
+    gameTimeStyle: {
+        position: "absolute",
+        color: "white",
+        textAlign: "center",
+        width: "100%",
+        top: 60,
+        fontSize: 30,
+        fontWeight: "bold"
+    },
+      teamALogoStyle: {
+        width: 60,
+        height: 60,
+        position: "absolute",
+        top: "11%",
+        left: "20%",
+      },
+        teamBLogoStyle: {
+        width: 60,
+        height: 60,
+        position: "absolute",
+        top: "12.5%",
+        left: "63%",        
+      },
 })
